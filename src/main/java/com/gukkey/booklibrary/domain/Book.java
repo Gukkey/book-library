deea147abca58706e8b561869ed367dc4a9b9b70
Gukkey <gukkey@protonmail.com>
2024-04-23 00:56:34 +0530

    feat(domain): created entities

--- src/main/java/com/gukkey/booklibrary/domain/Book.java
+++ src/main/java/com/gukkey/booklibrary/domain/Book.java
@@ -1,3 +1,39 @@
 package com.gukkey.booklibrary.domain;
 
-public class Book {}
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Year;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "book")
+public class Book {
+  @Id
+  @Column(name = "id", nullable = false, updatable = false)
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+
+  @Column(name = "title", nullable = false)
+  private String title;
+
+  @ManyToOne
+  @JoinColumn(name = "author_id", referencedColumnName = "id")
+  private Author author;
+
+  @Column(name = "isbn", nullable = false, unique = true)
+  private String isbn;
+
+  @Column(name = "publication_year", nullable = false)
+  private Year year;
+
+  @OneToOne(mappedBy = "book", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+  private Rental rental;
+}
--- src/main/java/com/gukkey/booklibrary/domain/Rental.java
+++ src/main/java/com/gukkey/booklibrary/domain/Rental.java
@@ -1,3 +1,30 @@
 package com.gukkey.booklibrary.domain;
 
-public class Rental {}
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "rental")
+public class Rental {
+    @OneToOne
+    @JoinColumn(name = "book_id", referencedColumnName = "id")
+    private Book book;
+
+    @Column(name = "renter_name")
+    private String renterName;
+
+    @Column(name = "rental_date")
+    private LocalDate rentalDate;
+
+    @Column(name = "return_date")
+    private LocalDate returnDate;
+}
