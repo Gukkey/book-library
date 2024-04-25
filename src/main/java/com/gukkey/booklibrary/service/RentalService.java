a9baf955a9d873837ecb20e9119cc8c07ad8b829
Gukkey <gukkey@protonmail.com>
2024-04-25 22:56:08 +0530

    refactor(mapper): added methods for return rental, and checking overdue rentals

--- src/main/java/com/gukkey/booklibrary/service/RentalService.java
+++ src/main/java/com/gukkey/booklibrary/service/RentalService.java
@@ -1,6 +1,7 @@
 package com.gukkey.booklibrary.service;
 
 import com.gukkey.booklibrary.model.dto.RentalDTO;
+import com.gukkey.booklibrary.model.res.RentalListResponse;
 import com.gukkey.booklibrary.model.res.RentalResponse;
 import org.springframework.http.ResponseEntity;
 
@@ -9,5 +10,9 @@ import java.util.UUID;
 public interface RentalService {
   ResponseEntity<RentalResponse> createRental(RentalDTO rentalDTO);
 
-  ResponseEntity<RentalResponse> retriveRental(UUID bookId);
+  ResponseEntity<RentalResponse> retrieveRental(UUID bookId);
+
+  ResponseEntity<RentalListResponse> checkOverdueRentals();
+
+  ResponseEntity<RentalResponse> returnRental(UUID bookId);
 }
