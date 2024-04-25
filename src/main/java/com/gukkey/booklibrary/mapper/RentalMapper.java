06476efb33a5d9f034f447a8502b673db35bccf3
Gukkey <gukkey@protonmail.com>
2024-04-25 22:55:35 +0530

    refactor(mapper): added docs and added another object to the builder

--- src/main/java/com/gukkey/booklibrary/mapper/RentalMapper.java
+++ src/main/java/com/gukkey/booklibrary/mapper/RentalMapper.java
@@ -1,17 +1,16 @@
 package com.gukkey.booklibrary.mapper;
 
-import com.gukkey.booklibrary.domain.Author;
 import com.gukkey.booklibrary.domain.Book;
 import com.gukkey.booklibrary.domain.Rental;
 import com.gukkey.booklibrary.model.dto.RentalDTO;
-import com.gukkey.booklibrary.repository.AuthorRepository;
 import com.gukkey.booklibrary.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.Optional;
 
+@Component
 public class RentalMapper {
   private final BookRepository bookRepository;
 
@@ -20,17 +19,23 @@ public class RentalMapper {
     this.bookRepository = bookRepository;
   }
 
+  /**
+   * Converts a RentalDTO object to a Rental object.
+   *
+   * @param rentalDTO the RentalDTO object to be converted
+   * @return the converted Rental object
+   * @throws IllegalArgumentException if the book with the given bookId is not found
+   */
   public Rental toRental(RentalDTO rentalDTO) {
     Optional<Book> book = bookRepository.findById(rentalDTO.getBookId());
     if (book.isEmpty()) {
       throw new IllegalArgumentException("Book not found in this id");
     }
-    LocalDate returnDate = rentalDTO.getRentalDate().plusDays(14);
     return Rental.builder()
-        .book(book.get())
+        .bookId(rentalDTO.getBookId())
         .renterName(rentalDTO.getRenterName())
-        .rentalDate(rentalDTO.getRentalDate())
-        .returnDate(returnDate)
+        .rentalDate(LocalDate.now())
+        .returnDate(LocalDate.now().plusDays(14))
         .build();
   }
 }
